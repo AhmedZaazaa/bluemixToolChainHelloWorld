ee83280d3f29e9f953e552063a91d7db7e42fc93
AhmedZaazaa <ahmed-zaaza@hotmail.com>
2017-06-16 12:54:58 +0000

    Trying to integrate facebook

--- app.js
+++ app.js
@@ -27,3 +27,49 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
+});
+
+/* Handling all messenges */
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
+
+const request = require('request');
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: 'EAACwHGVDi1sBAJvOo8jWEDKfZBYpjsqqGguJg4ZAXsOj5aAnPU2NslXmRzsi4lf6RJ110yvaEnZAwxhmqcHSqQHTZCYHD99Gafbu2h2NU2OpiwRqRAXvYWOpnPZC8YYh99JgBNJ9aQxOdxD3O8vKDRaplhnAAJ18wCdWB8bnNlgZDZD'},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
