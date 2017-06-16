1df34d3b3277688a150cfd4a32b37344c3ce8a4f
AhmedZaazaa <ahmed-zaazaa@hotmail.com>
2017-06-16 15:07:22 +0200

    Used Strict mode in sendMessage function
    added  "use strict"; on line 57.

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.post('/webhook', (req, res) => {
 const request = require('request');
 
 function sendMessage(event) {
+  "use strict";
   let sender = event.sender.id;
   let text = event.message.text;
 
